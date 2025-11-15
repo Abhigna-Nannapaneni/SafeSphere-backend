695751e3e76b8a2c33b42f8e26bf447922fef66d
Abhigna-Nannapaneni <nabhigna29@gmail.com>
2025-11-15 18:45:02 +0530

    SafeSphere Backend

---
+++ src/main/java/b10/com/example/b10/B10Application.java
@@ -0,0 +1,14 @@
+package b10.com.example.b10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(B10Application.class, args);
+		System.out.println("project b10");
+	}
+
+}
