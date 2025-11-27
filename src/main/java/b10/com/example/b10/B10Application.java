52845c87e5d4c291ef9992935f181b74233fb576
Abhigna Nannapaneni <158666377+Abhigna-Nannapaneni@users.noreply.github.com>
2025-11-27 19:38:15 +0530

    Update B10Application.java

--- src/main/java/b10/com/example/b10/B10Application.java
+++ src/main/java/b10/com/example/b10/B10Application.java
@@ -2,13 +2,12 @@ package b10.com.example.b10;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("b10.com.example.b10")  // ADD THIS LINE
 public class B10Application {
-
-	public static void main(String[] args) {
-		SpringApplication.run(B10Application.class, args);
-		System.out.println("project b10");
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(B10Application.class, args);
+    }
 }
