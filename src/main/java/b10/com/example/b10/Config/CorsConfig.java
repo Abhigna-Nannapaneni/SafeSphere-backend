49ce39a9f0c4eb4eb487df346cbc4648886dc7d1
Abhigna Nannapaneni <158666377+Abhigna-Nannapaneni@users.noreply.github.com>
2025-11-27 23:13:37 +0530

    Update CorsConfig.java

--- src/main/java/b10/com/example/b10/Config/CorsConfig.java
+++ src/main/java/b10/com/example/b10/Config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("http://localhost:30082")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // ← ADD OPTIONS
+                        .allowedMethods("*") // ← ADD OPTIONS
                         .allowedHeaders("*")
                         .allowCredentials(true); // ← ADD THIS TOO
             }
